f40407eb69576b560eb30337a0e8a260c25f645d
SayakaIkki <158832487+sayaka-ikki@users.noreply.github.com>
2024-04-17 15:34:56 +0000

    [Finish]4-24-3 ｱｿｼｴｰｼｮﾝUserﾓﾃﾞﾙ⇔Bookﾓﾃﾞﾙ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+
+  resources :users, only: [:index, :edit]
+  get '/users/show', to: 'users#show'
+
+  resources :books, only:[:new, :index, :show, :edit]
+  root to: "homes#top"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
