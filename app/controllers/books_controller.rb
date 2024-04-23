aaa36ef7608c01dd42b8450feffd96ec003240e4
SayakaIkki <158832487+sayaka-ikki@users.noreply.github.com>
2024-04-23 15:27:25 +0000

    [update]4-24-6 user_edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,18 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def create
-    @book = Book.new(book_params)
-
-    if @book.save
-      flash[:notice] = "You have created book successfully."
-      redirect_to books_path
-    else
-      @books = Book.all
-      render :index
-    end
+  @book = Book.new(book_params)
+  if @book.save
+    flash[:notice] = "You have created book successfully."
+    redirect_to books_path
+  else
+    flash[:alert] = @book.errors.full_messages.join(", ")
+    @books = Book.all
+    render :index
+  end
   end
 
   def new
